534d6c90c7deb9385070c86c573cac68f42855e6
Sagar-Kr-Thapa <076bct062.sagar@pcampus.edu.np>
2022-07-10 15:11:52 +0545

    task-manager production final phase

--- task-manager/app.js
+++ task-manager/app.js
@@ -4,14 +4,18 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 
+const errorHandler = require('./middleware/error-handler')
+const notFound = require('./middleware/not-found')
 
-const port=3000
+const port = process.env.PORT || 3000
 
 app.use(express.static('./public'))
 app.use(express.json())
 
 app.use('/api/v1/tasks',tasks)
 
+app.use(notFound)
+app.use(errorHandler)
 
 const start=async()=>{
     try {
