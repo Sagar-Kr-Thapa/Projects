c93ea87f5083d2e786a6f07ac522daea5ee22416
Sagar-Kr-Thapa <076bct062.sagar@pcampus.edu.np>
2022-07-14 17:56:23 +0545

    jwt basics covered

---
+++ JWT-Basics/middleware/auth.js
@@ -0,0 +1,24 @@
+const {UnauthenticatedError}= require("../errors");
+const jwt = require('jsonwebtoken');
+const authMiddleware = async (req,res,next)=>{
+    const authHeader = req.headers.authorization;
+
+    if( !authHeader || !authHeader.startsWith('Bearer '))
+    {
+        throw new UnauthenticatedError('No token provided');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SEC);
+        const { id,username } = decoded;
+        req.user = {id,username};
+        
+        next();
+    } catch (error) {
+        throw new UnauthenticatedError('No authorized access to this route');
+    }
+}
+
+module.exports = authMiddleware;
